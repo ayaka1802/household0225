11d61e1d7521111a2408a9d0cfe38e8b5b21e18b
AyakaOkimura <okkiayrskr@gmail.com>
2024-02-28 21:57:25 +0900

    0228 21:57

--- kakeibo_app.py
+++ kakeibo_app.py
@@ -2,13 +2,20 @@ import streamlit as st
 import data
 import const
 import datetime
+import re
+import altair as alt
+import pandas as pd
 st.set_page_config(**const.SET_PAGE_CONFIG)
 st.markdown(const.HIDE_ST_STYLE, unsafe_allow_html=True)
 ##selected = option_menu(**const.OPTION_MENU_CONFIG)
 
 st.title('家計簿アプリ')
 tab1, tab2, tab3, tab4 = st.tabs(["支出入力", "収入入力", "一覧表", "分析"])
-
+all_type = ['家賃','電気','ガス','水道',
+                                 'おそとあそび費','おうちあそび費','外食費',
+                                 'カフェ費','日用品費','スーパー費','コンビニ費',
+                                 '通信費','ひとりごはん費','交通費',
+                                 '服費','美容費','勉強費','必須費','娯楽費','積立NISA']
 with tab1:
     st.write('### 支出入力')
 
@@ -59,15 +66,15 @@ with tab2:
 
 with tab3:
     st.write('### 一覧表')
-    search_all = st.multiselect('分類名',
-                                ['家賃','電気','ガス','水道',
-                                 'おそとあそび費','おうちあそび費','外食費',
-                                 'カフェ費','日用品費','スーパー費','コンビニ費',
-                                 '通信費','ひとりごはん費','交通費',
-                                 '服費','美容費','勉強費','必須費','娯楽費','積立NISA'],
-                                 default='家賃')
+    search_all = st.multiselect('分類名',all_type,default=all_type)
+    date = ("{}/{:02}".format(y,m) for y in range(2024,2050) for m in range(1,13))
+    search_month=st.selectbox('年月：',date)
+    search_month = re.sub(r'/', '', search_month)  # "/"を削除
+    #year_month = "".join(date.split("-")[:2])
     df = data.show_all_data()
     df = df[(df["費目2"].isin(search_all))]
+    df = df.loc[df["日付"].str.replace("-", "").str.startswith(search_month)]
+    #df = df["".join(df["日付"].split("-")[:2])==search_month]
     st.dataframe(df,use_container_width=True, hide_index=True)
 
 with tab4:
@@ -81,11 +88,34 @@ with tab4:
     df_two.reset_index(level='費目1', inplace=True)
     df_two = df_two.drop(columns=['費目1'])
     st.dataframe(df_two,use_container_width=True)
+    st.write('支出推移グラフ')
+    df_two_2 = df_two.T.reset_index()
+    # データフレームを積み上げ形式に変換
+    df_melted = df_two_2.melt(id_vars=["年月"], var_name="支出項目", value_name="金額")
+    # 積み上げ棒グラフを作成
+    chart = alt.Chart(df_melted).mark_bar().encode(
+    x='年月',
+    y='金額',
+    color=alt.Color(
+            "支出項目",
+            scale=alt.Scale(
+                range=["#FF9999","#99FF99","#9999FF","#FFCC66","#FF99CC","#99CCFF","#CC99FF","#66CCCC","#FFFF99","#FF99CC","#99FFCC"]
+            )),
+    tooltip=['年月', '支出項目', '金額']
+    ).properties(
+    width=500
+    )
+    st.altair_chart(chart,use_container_width=True)
+
+    
+
+
     st.write('あやたん費')
     df_aya = df.loc[df.index.get_level_values('費目1') == 'あやたん費']
     df_aya.reset_index(level='費目1', inplace=True)
     df_aya = df_aya.drop(columns=['費目1'])
     st.dataframe(df_aya,use_container_width=True)
+
     st.write('だいすけ費')
     df_dai = df.loc[df.index.get_level_values('費目1') == 'だいすけ費']
     df_dai.reset_index(level='費目1', inplace=True)
